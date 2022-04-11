14003ef922f269ad354a47873599aaf1473b7d4d
SuzukiHanari <vgnlyoko@gmail.com>
2022-04-12 02:05:54 +0800

    update wp

--- emdm/wireproxy/build.sh
+++ emdm/wireproxy/build.sh
@@ -2,5 +2,5 @@
 set -ex
 git clone https://github.com/octeep/wireproxy
 cd ./wireproxy
-CGO_ENABLED=0 go build -ldflags "-s -w" -o wireproxy
+CGO_ENABLED=0 go build -ldflags "-s -w" -o wireproxy ./cmd/wireproxy/main.go
 upx wireproxy
