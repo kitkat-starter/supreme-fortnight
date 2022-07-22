23b41159879e1e1016f719bb0ee4138376727769
SuzukiHanari <vgnlyoko@gmail.com>
2022-07-23 01:48:13 +0800

    some update

---
+++ gsipct/steam/source-common/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Wait for the container to fully initialize
+sleep 1
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# 因为这是一个基础镜像,所以此处不做任何的操作.
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
