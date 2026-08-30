f8a513fdf0fd3b92a85c6b9700d237b1c139b242
SuzukiHanari <17796830+chisaato@users.noreply.github.com>
2026-08-30 22:27:25 +0800

    feat: sing-box add tags

--- builds/stack/singbox/build.sh
+++ builds/stack/singbox/build.sh
@@ -12,5 +12,5 @@ git fetch --tags sekai
 VERSION=$(CGO_ENABLED=0 go run ./cmd/internal/read_tag)
 echo version=$VERSION > ./version
 
-TAGS="with_quic,with_dhcp,with_wireguard,with_shadowsocksr,with_ech,with_utls,with_acme,with_reality_server,with_clash_api,with_gvisor"
+TAGS="with_quic,with_grpc,with_dhcp,with_wireguard,with_utls,with_acme,with_clash_api,with_v2ray_api,with_gvisor,with_tailscale,with_ccm,with_ocm,with_naive_outbound,with_cloudflared,badlinkname,tfogo_checklinkname0"
 go build -o sing-box -v -trimpath -ldflags "-checklinkname=0 -X 'github.com/sagernet/sing-box/constant.Version=${VERSION}' -s -w -buildid=" -tags "${TAGS}" ./cmd/sing-box
